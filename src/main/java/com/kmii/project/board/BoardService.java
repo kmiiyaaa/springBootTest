88d6b2a3a115a00c783f1823552787f8fad62ce7
kmiiyaaa <ckm1533@naver.com>
2025-09-15 15:18:33 +0900

    게시글 최신글 조회 정렬 추가

--- src/main/java/com/kmii/project/board/BoardService.java
+++ src/main/java/com/kmii/project/board/BoardService.java
@@ -1,12 +1,14 @@
 package com.kmii.project.board;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.kmii.project.DataNotFoundException;
@@ -22,7 +24,9 @@ public class BoardService {
 	// 모든 게시글 가져오기
 	public Page<Board> getList(int page){  
 		
-		Pageable pageable = PageRequest.of(page, 10);
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("bdate"));		
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 		return boardRepository.findAll(pageable);
 		
 	}
