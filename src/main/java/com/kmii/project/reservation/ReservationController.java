234ff01f4d55e21015dcb5c628b99073f169aa95
kmiiyaaa <ckm1533@naver.com>
2025-09-16 17:51:48 +0900

    수정추가

--- src/main/java/com/kmii/project/reservation/ReservationController.java
+++ src/main/java/com/kmii/project/reservation/ReservationController.java
@@ -6,21 +6,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kmii.project.user.SiteUser;
 import com.kmii.project.user.UserService;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
+
 
 @Controller
 @RequestMapping("/reservation")
